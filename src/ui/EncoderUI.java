43cf8489323f69ac1f29a0ba11ab9598af081e94
DieterDraelants <dieter.draelants@student.ucll.be>
2019-09-25 16:16:05 +0200

    comit

--- src/ui/EncoderUI.java
+++ src/ui/EncoderUI.java
@@ -5,7 +5,37 @@ import java.awt.*;
 
 public class EncoderUI {
     public static void main(String[] args) {
-        JOptionPane.showInputDialog("Wat wil je versleutelen");
+        String string = JOptionPane.showInputDialog("Wat wil je versleutelen");
+
+        //slaat string op in
+        String toBeEncoded = string;
+
+        int choice = -1;
+        while (choice != 0) {
+            String choiceString = JOptionPane.showInputDialog("Hoe wil je het versleutelen \n1.homo\n2.fiel");
+
+            if (choiceString == null) {
+                choice = 0;
+                continue;
+            }
+            if (choiceString.isEmpty()) {
+                continue;
+            }
+            try {
+                choice = Integer.parseInt(choiceString);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Invalid menu choice: \"" + choiceString + "\", try again.");
+                continue;
+            }
+            if (choice == 1) {
+                //yeet1
+                JOptionPane.showMessageDialog(null, "YEET1");
+            } else if (choice == 2) {
+                //yeet2
+                JOptionPane.showMessageDialog(null, "YEET2");
+            }
+
+        }
     }
 
 }
