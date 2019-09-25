4e1cccb89a97642038dba46e2bbd6cc494b3c52b
DieterDraelants <dieter.draelants@student.ucll.be>
2019-09-26 00:15:06 +0200

    Kheb er iet van gemaakt check maar of ge het nice vindt

--- src/ui/EncoderUI.java
+++ src/ui/EncoderUI.java
@@ -5,21 +5,45 @@ import domain.EncoderContext;
 import domain.MirrorEncoder;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class EncoderUI {
     public static void main(String[] args) {
-        String string = JOptionPane.showInputDialog("Wat wil je versleutelen");
         //slaat string op in
-        String toBeEncoded = string;
-        keuzeversleutel(toBeEncoded);
+        String toBeModified = JOptionPane.showInputDialog("Met welke zin/woord wil je iets doen?");
+
+        keuzeversleutel(toBeModified);
         //Vraag voor encode of decode
 
     }
-        //Keuze ding voor versleutelingstype
-        private static void keuzeversleutel(String toBeEncoded) {
+
+    //Keuze ding
+    private static String KeuzeVerOfNiet() {
+        int choice = -1;
+        while (choice != 0) {
+            String choiceString = JOptionPane.showInputDialog("\n1.Encode\n2.Decode");
+
+            if (choiceString == null || choiceString.isEmpty()) {
+                continue;
+            }
+            try {
+                choice = Integer.parseInt(choiceString);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Invalid menu choice: \"" + choiceString + "\", try again.");
+                continue;
+            }
+            if (choice == 1) {
+                return "encode";
+            } else if (choice == 2) {
+                return "decode";
+            }
+
+        }
+        return "";
+    }
+
+
+    //Keuze ding voor versleutelingstype
+    private static void keuzeversleutel(String toBeEncoded) {
             int choice = -1;
             while (choice != 0) {
                 String choiceString = JOptionPane.showInputDialog("Op welke manier? \n1.CaeserSalad\n2.MirrorView");
@@ -40,11 +64,19 @@ public class EncoderUI {
                 if (choice == 1) {
                     //yeet1
                     EncoderContext context = new EncoderContext(new CeasarEncoder());
-                    JOptionPane.showMessageDialog(null, context.encode(toBeEncoded));
+                    if (KeuzeVerOfNiet().equals("encode")) {
+                        JOptionPane.showMessageDialog(null, context.encode(toBeEncoded));
+                    } else {
+                        JOptionPane.showMessageDialog(null, context.decode(toBeEncoded));
+                    }
                 } else if (choice == 2) {
                     //yeet2
                     EncoderContext context = new EncoderContext(new MirrorEncoder());
-                    JOptionPane.showMessageDialog(null, context.encode(toBeEncoded));
+                    if (KeuzeVerOfNiet().equals("encode")) {
+                        JOptionPane.showMessageDialog(null, context.encode(toBeEncoded));
+                    } else {
+                        JOptionPane.showMessageDialog(null, context.decode(toBeEncoded));
+                    }
                 }
 
             }
