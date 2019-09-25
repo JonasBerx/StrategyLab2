9a40cb450d41cd097dde841f8bdbcb2b9b9064c0
DieterDraelants <dieter.draelants@student.ucll.be>
2019-09-25 15:59:43 +0200

    comit

--- src/ui/EncoderUI.java
+++ src/ui/EncoderUI.java
@@ -5,14 +5,7 @@ import java.awt.*;
 
 public class EncoderUI {
     public static void main(String[] args) {
-        Scene s = new Scene(root, 300, 300, Color.BLACK);
-
-        final Canvas canvas = new Canvas(250,250);
-
-
-
-        Label label1 = new Label("Name:");
-        TextField textField = new TextField();
+        JOptionPane.showInputDialog("Wat wil je versleutelen");
     }
 
 }
