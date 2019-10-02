0f29bcf5dd507c05bedf1f3573febb852cb2c996
Pieter Fiers <Ubipo.Skippy@gmail.com>
2019-10-02 15:40:33 +0200

    Added JavafxUI

---
+++ src/domain/SecretCode.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class SecretCode {
+    private String text;
+
+    public SecretCode(String text) {
+        this.text = text;
+    }
+}
---
+++ src/ui/EncoderUIJavafx.java
@@ -0,0 +1,35 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class EncoderUIJavafx extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        VBox root = new VBox();
+        Label label = new Label("Test");
+        ComboBox<String> encoderComboBox = new ComboBox<>(FXCollections.observableList(Arrays.asList(
+                "Ceasar",
+                "Mirror"
+        )));
+        root.getChildren().addAll(label, encoderComboBox);
+        Scene mainScene = new Scene(root, 250, 40);
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+}
